32ef73917353c3e0dbc2769cca645ff27b1e67cc
Liubov555 <101938652+Liubov555@users.noreply.github.com>
2023-01-29 16:41:52 +0200

    Додані зміни

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -5,15 +5,19 @@ import { Overlay, ModalImg } from "./Modal.styled";
 export default function Modal({ toggleModal, largeImage }) {
 
     useEffect(() => {
+
         const handleKeyDown = evt => evt.code === 'Escape' && toggleModal();
+
         window.addEventListener('keydown', handleKeyDown);
+
         return () => {
-            window.removeEventListener('keydown', this.handleKeyDown);
+            window.removeEventListener('keydown', handleKeyDown);
         }
+
     }, [toggleModal]);
 
     const handleBackdropClick = evt => {
-        evt.target === evt.current.Target && toggleModal();
+        evt.target === evt.currentTarget && toggleModal();
     };
 
     return (
